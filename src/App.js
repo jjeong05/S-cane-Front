8defdaab517e6332de21cf533e4f6fe257a7d7ef
songjeongmin <ssong6299595@gmail.com>
2022-11-16 14:09:05 +0900

    warning 미완

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
+import './css/App.css';
+import './css/warning.css'
 import Login from "./login";
 import Header from './header';
 import Map from "./map";
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { BrowserRouter, BrowserRouter as Router, Link, Route } from "react-router-dom";
 import Pin from './pin';
 import Notification from './notice';
@@ -11,6 +12,16 @@ import Warning from './warning';
 
 
 function App() {
+  const [shock, setShock] = useState(false);
+  useEffect(()=>{
+    if(shock){
+    setShock(true);
+    console.log("충격감지");
+    shock(false);
+    }
+  },[])
+  
+
   return (      
     <div className="App">
     <BrowserRouter>
@@ -19,7 +30,9 @@ function App() {
         <Map>
         <Route exact path='/' component={Pin} key='1'></Route>
         </Map>
-        <Warning/>
+
+        {setShock ? '숨김':'보임'}
+        {setShock && <Warning/>}
     </BrowserRouter>
     </div>
   );
--- src/warning.js
+++ src/warning.js
@@ -1,17 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 
 function Warning(props) {
+    const tracking_location = () =>{
+       
+    }
+    useEffect(()=>{
+        let timer = setTimeout(()=>{
+            props.setShow(false);
+        }, 2000);
+        return ()=>{clearTimeout(timer)}
+    }, []);
+
     return(
         <div id='warn-back'>
            <div id="warning-container">
             <div id="warning-content">
                 <img src="img/Error outline.png" alt="warn_icon"/>
-                <h2>지팡이 추락</h2>
+                <h2>지팡이 충격 감지</h2>
                 {/* <div id="mini-line"></div>             */}
-                <p>지팡이가 바닥에 떨어졌습니다.</p>
-                <button>위치 확인</button>  
+                <p>지팡이에 큰 충격이 감지되었습니다.</p>
+                <button onClick={()=>tracking_location()}>위치 확인</button>  
             </div>  
         </div> 
         </div>
